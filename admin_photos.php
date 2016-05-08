e17bc9408a910bfcfb4542143aa16149b34bd268
Vincent Bas <Vincent Bas>
2016-05-08 13:32:38 +0200

    Modification du CSS d'admin_users pour afficher deux utilisateurs ou deux photos sur la même ligne

--- admin_photos.php
+++ admin_photos.php
@@ -1,112 +1,133 @@
 <?php
+//RECUPERATION DES VARIABLES DE SESSION
 session_start();
-//Connexion base de données
-$bdd = new PDO('mysql:host=localhost;dbname=phplogin', 'root', '');
 include_once('cookie_connect.php');	
-if(isset($_GET['id']) AND $_GET['id'] > 0) 
+
+try
 {
-$getid = intval($_GET['id']);
-$requser = $bdd->prepare("SELECT * FROM users WHERE id ='$getid'");
-$requser->execute(array($getid));
-$userinfo = $requser->fetch();
+	//CONNEXION BDD
+	$bdd = new PDO('mysql:host=localhost;dbname=phplogin;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	die('Erreur : '.$e->getMessage());
+}
+
+//TEST: VERIFICATION QUE LA VARIABLE ID EXISTE ET SUPERIEUR A 0
+if(isset($_GET['id']) AND $_GET['id'] > 0)
+{
+	//SECURISER LA VARIABLE CONVERSION DE CE MET L'UTILISATEUR EN NOMBRE
+	$getid = intval($_GET['id']);
+	
+	//REQUETE: SELECTIONNE LES INFORMATIONS DE L'UTILISATEUR CONNECTE
+	$requser = $bdd->prepare("SELECT * FROM users WHERE id ='$getid'");
+	$requser->execute(array($getid));
+	
+	//(fetch()): FONCTION QUI RECUPERE LES INFOS PROPPRE A L'UTILISATEUR CONNECTE
+	$userinfo = $requser->fetch();
 ?>
 <!DOCTYPE html>
 <html>
 
-<head>
-    <title> AMSTRAGRAM </title>
-    <meta charset="utf-8"/>
-    <link rel="stylesheet" href="admin.css"/>
-</head>
+	<head>
+		<title> AMSTRAGRAM </title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="admin_users.css"/>
+	</head>
 
-<body>
-
-	<!-- barre du haut, curieusement il n'y a que sur cette page que le hover fonctionne. Je vais regler ca -->
-	<h3> AMSTRAMGRAM </h3>
-	<h6> Espace Administrateur</h6>
-	<nav>
-		<ul>
-			<li>
-				<a href="admin.php">informations</a>
-			</li>
-			<li>
-				<a href="admin_users.php">utilisateurs</a>
-			</li>
-			<li>
-				<a href="admin_photos.php">photos</a>
-			</li>
-			<li>
-				<input type="text" placeholder="rechercher"/>
-				<input type="submit" value="OK"/>
-			</li>
-			<li>
-				<a href="deconnexion.php">deconnexion</a>
-			</li>
-		</ul>
-	</nav>
-
-	<ul id="mylist">
-		<?php 
-		if(!empty($userinfo['avatar']))
-		{
-			?>
-			<img src="users/avatar/<?php echo $userinfo['avatar'];?>" style="width:150px;height:150px;"/>
-			<?php
-		}
-		?>
-		<li>
-			<?php echo $userinfo['pseudo'];?>
-		</li>
-		<li>
-			<?php echo $userinfo['date_naissance'];?>
-		</li>
-		<li>
-			<?php echo $userinfo['pays'];?>
-		</li>
-	</ul>
-	<!-- liste des 3 photos-->
-	<!-- chaque element (article) est constitué d'une photo, d'un sous titre (h4), et d'une légenre (p)-->
-	<!--le fond noir, c'est juste pour me reperer-->
-
-	<div class="listphotos">
+	<body>
+		<h3> AMSTRAMGRAM </h3>
+		<h6> Espace Administrateur</h6>
 		
-					
-			<?php
-			$photos = $bdd->query("SELECT * FROM photo");
-
-			// On affiche chaque entrée une à une
-			while ($photos_data = $photos->fetch())
+		<!--MENU-->
+		<nav>
+			<ul>
+				<li>
+					<a href="admin.php">informations</a>
+				</li>
+				<li>
+					<a href="admin_users.php">utilisateurs</a>
+				</li>
+				<li>
+					<a href="admin_photos.php">photos</a>
+				</li>
+				<li>
+					<input type="text" placeholder="rechercher"/>
+					<input type="submit" value="OK"/>
+				</li>
+				<li>
+					<a href="deconnexion.php">deconnexion</a>
+				</li>
+			</ul>
+		</nav>
+		
+		<!--PROFIL: INFORMATION PROPRE A L'ADMINISTRATEUR-->
+		<ul id="mylist">
+		</br>
+			<?php 
+			if(!empty($userinfo['avatar']))
 			{
-			?>
-			<article class="pt">
-			
-			<h4>
-			<br/>
-				<?php 
-				if(!empty($photos_data['img']))
-				{
-					?>
-					<img src="photos/<?php echo $photos_data['img'];?>" style="width:100px;height:100px;"/><br/>
-					<?php
-				}
 				?>
-				Nom Image : <?php echo $photos_data['NomImage']; ?><br />
-				Date: <?php echo $photos_data['date']; ?><br />
-				Heure : <?php echo $photos_data['heure']; ?><br/>
-				Lieu : <?php echo $photos_data['lieu']; ?><br/>
-				Description : <?php echo $photos_data['description']; ?><br/>
-				<div class="row">
-					<a href="delete_photos.php?id=<?= $photos_data['id']?>">Suppression Photos</a>
-				</div>
-			<br/>
-			</h4>
-			
-			</article>
-			<?php
+				<!--AVATAR ADMINISTRATEUR-->
+				<img src="users/avatar/<?php echo $userinfo['avatar'];?>" style="width:150px;height:150px;"/>
+				<?php
 			}
-		?>
-	</div>
-</body>
+			?>
+			<li>
+				<?php echo $userinfo['pseudo'];?>
+			</li>
+			<li>
+				<?php echo $userinfo['date_naissance'];?>
+			</li>
+			<li>
+				<?php echo $userinfo['pays'];?>
+			</li>
+			</br>
+		</ul>
+		<div class="listphotos">	
+			<table>		
+				<?php
+				//REQUETE: SELECTION DE TOUTES LES PHOTOS DE LA TABLE PHOTO
+				$photos = $bdd->query("SELECT * FROM photo");
+				$nb_result=$photos->rowCount();
+				
+				// ON AFFICHE LES ENTREES UNE A UNE
+				while ($photos_data = $photos->fetch())
+				{
+				?>
+				
+				<article class="pt">				
+					<h4>
+					<br/>
+						<?php 
+						if(!empty($photos_data['img']))
+						{
+							?>
+							<!--AFFICHAGE IMAGE-->
+							<img src="photos/<?php echo $photos_data['img'];?>" style="width:100px;height:100px;"/><br/>
+							<?php
+						}
+						?>
+						<!--AFFICHAGE INFORMATIONS CONCERNANT LA PHOTO-->
+						Nom Image : <?php echo $photos_data['NomImage']; ?><br />
+						Date: <?php echo $photos_data['date']; ?><br />
+						Heure : <?php echo $photos_data['heure']; ?><br/>
+						Lieu : <?php echo $photos_data['lieu']; ?><br/>
+						Description : <?php echo $photos_data['description']; ?><br/></br>
+						
+						<!--LIEN SUPPRESSION PHOTOS-->
+						<div class="row">
+							<a href="delete_photos.php?id=<?= $photos_data['id']?>"><img src="images/others/supprimer.png" width="100px;"/></a>
+						</div>
+					<br/>
+					</h4>			
+				</article>
+				<?php
+				}
+			?>
+			</table>
+		</div>
+	</body>
 </html>
 <?php
 }
--- admin_users.php
+++ admin_users.php
@@ -1,109 +1,128 @@
 <?php
+//RECUPERATION DES VARIABLES DE SESSION
 session_start();
-//Connexion base de données
-$bdd = new PDO('mysql:host=localhost;dbname=phplogin', 'root', '');
 include_once('cookie_connect.php');	
-if(isset($_GET['id']) AND $_GET['id'] > 0) 
+
+try
 {
-$getid = intval($_GET['id']);
-$requser = $bdd->prepare("SELECT * FROM users WHERE id ='$getid'");
-$requser->execute(array($getid));
-$userinfo = $requser->fetch();
+	//CONNEXION BDD
+	$bdd = new PDO('mysql:host=localhost;dbname=phplogin;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	die('Erreur : '.$e->getMessage());
+}
+
+//TEST: VERIFICATION QUE LA VARIABLE ID EXISTE ET SUPERIEUR A 0
+if(isset($_GET['id']) AND $_GET['id'] > 0)
+{
+	//SECURISER LA VARIABLE CONVERSION DE CE MET L'UTILISATEUR EN NOMBRE
+	$getid = intval($_GET['id']);
+	
+	//REQUETE: SELECTIONNE LES INFORMATIONS DE L'UTILISATEUR CONNECTE
+	$requser = $bdd->prepare("SELECT * FROM users WHERE id ='$getid'");
+	$requser->execute(array($getid));
+	
+	//(fetch()): FONCTION QUI RECUPERE LES INFOS PROPPRE A L'UTILISATEUR CONNECTE
+	$userinfo = $requser->fetch();
 ?>
 <!DOCTYPE html>
 <html>
 
-<head>
-    <title> AMSTRAGRAM </title>
-    <meta charset="utf-8"/>
-    <link rel="stylesheet" href="admin.css"/>
-</head>
+	<head>
+		<title> AMSTRAGRAM </title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="admin_users.css"/>
+	</head>
 
-<body>
-
-	<!-- barre du haut, curieusement il n'y a que sur cette page que le hover fonctionne. Je vais regler ca -->
-	<h3> AMSTRAMGRAM </h3>
-	<h6> Espace Administrateur</h6>
-	<nav>
-		<ul>
-			<li>
-				<a href="admin.php">informations</a>
-			</li>
-			<li>
-				<a href="admin_users.php">utilisateurs</a>
-			</li>
-			<li>
-				<a href="admin_photos.php">photos</a>
-			</li>
-			<li>
-				<input type="text" placeholder="rechercher"/>
-				<input type="submit" value="OK"/>
-			</li>
-			<li>
-				<a href="deconnexion.php">deconnexion</a>
-			</li>
-		</ul>
-	</nav>
-
-	<ul id="mylist">
-		<?php 
-		if(!empty($userinfo['avatar']))
-		{
-			?>
-			<img src="users/avatar/<?php echo $userinfo['avatar'];?>" style="width:150px;height:150px;"/>
-			<?php
-		}
-		?>
-		<li>
-			<?php echo $userinfo['pseudo'];?>
-		</li>
-		<li>
-			<?php echo $userinfo['date_naissance'];?>
-		</li>
-		<li>
-			<?php echo $userinfo['pays'];?>
-		</li>
-	</ul>
-	<!-- liste des 3 photos-->
-	<!-- chaque element (article) est constitué d'une photo, d'un sous titre (h4), et d'une légenre (p)-->
-	<!--le fond noir, c'est juste pour me reperer-->
-
-	<div class="listphotos">
+	<body>
+		<h3> AMSTRAMGRAM </h3>
+		<h6> Espace Administrateur</h6>
 		
-					
-					<?php
-					$users = $bdd->query("SELECT id, avatar, pseudo, date_naissance, pays FROM users");
+		<!--MENU-->
+		<nav>
+			<ul>
+				<li>
+					<a href="admin.php">informations</a>
+				</li>
+				<li>
+					<a href="admin_users.php">utilisateurs</a>
+				</li>
+				<li>
+					<a href="admin_photos.php">photos</a>
+				</li>
+				<li>
+					<input type="text" placeholder="rechercher"/>
+					<input type="submit" value="OK"/>
+				</li>
+				<li>
+					<a href="deconnexion.php">deconnexion</a>
+				</li>
+			</ul>
+		</nav>
 
-					// On affiche chaque entrée une à une
-					while ($users_data = $users->fetch())
-					{
-					?>
-					<article class="pt">
-					
-					<h4>
-					<br/>
-						<?php 
-						if(!empty($users_data['avatar']))
-						{
-							?>
-							<img src="users/avatar/<?php echo $users_data['avatar'];?>" style="width:100px;height:100px;"/><br/>
-							<?php
-						}
-						?>
-						Pseudo : <?php echo $users_data['pseudo']; ?><br />
-						Date de naissance : <?php echo $users_data['date_naissance']; ?><br />
-						Pays : <?php echo $users_data['pays']; ?><br/>
-
-						<div class="row">
-							<a href="delete_users.php?id=<?= $users_data['id']?>">Suppression Utilisateur</a>
-						</div>
-						<br/></h4>
-						</article>
-					<?php
-					}
+		<!--PROFIL: INFORMATION PROPRE A L'ADMINISTRATEUR-->
+		<ul id="mylist">
+		</br>
+			<?php 
+			if(!empty($userinfo['avatar']))
+			{
 				?>
-	</div>
-</body>
+				<!--AVATAR ADMINISTRATEUR-->
+				<img src="users/avatar/<?php echo $userinfo['avatar'];?>" style="width:150px;height:150px;"/>
+				<?php
+			}
+			?>
+			<li>
+				<?php echo $userinfo['pseudo'];?>
+			</li>
+			<li>
+				<?php echo $userinfo['date_naissance'];?>
+			</li>
+			<li>
+				<?php echo $userinfo['pays'];?>
+			</li>
+			</br>
+		</ul>
+
+		<div class="listphotos">		
+			<?php
+			//REQUETE: SELECTION DE TOUS LES UTILISATEURS DE LA TABLE USERS
+			$users = $bdd->query("SELECT id, avatar, pseudo, date_naissance, pays FROM users");
+
+			// ON AFFICHE LES ENTREES UNE A UNE
+			while ($users_data = $users->fetch())
+			{
+			?>						
+			<article class="pt">						
+				<h4>
+				<br/>
+					<?php 
+					if(!empty($users_data['avatar']))
+					{
+						?>
+						<!--AFFICHAGE DES AVATRS DES UTILISATEURS-->
+						<img src="users/avatar/<?php echo $users_data['avatar'];?>" style="width:100px;height:100px;"/><br/>
+						<?php
+					}
+					?>
+					<!--AFFICHAGE INFORMATIONS CONCERNANT L'UTILISATEUR-->
+					Pseudo : <?php echo $users_data['pseudo']; ?><br />
+					Date de naissance : <?php echo $users_data['date_naissance']; ?><br />
+					Pays : <?php echo $users_data['pays']; ?><br/></br>
+
+					<!--LIEN SUPPRESSION UTILISATEURS-->
+					<div class="row">
+						<a href="delete_users.php?id=<?= $users_data['id']?>"><img src="images/others/supprimer.png" width="100px;"/></a>
+					</div>
+					<br/>
+				</h4>
+			</article>
+			<?php
+			}
+			?>
+		</div>
+	</body>
 </html>
 <?php
 }
